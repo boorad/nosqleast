b15e742114d4c182d537018357debe3dc91ed2b7
Stefano J. Attardi <stefano@attardi.org>
2009-08-26 10:56:50 -0400

    Added tab completion in Firefox

--- nosql.js
+++ nosql.js
@@ -53,13 +53,22 @@ $(function() {
     $('#cursor').blink();
 
     $(document).bind('keypress', function(event) {
-        if (event.keyCode == 8) {
+        if (event.keyCode == 8) { // Backspace
             $('#command').text($('#command').text().substring(0, $('#command').text().length-1));
             return false;
-        } else if (event.keyCode == 13) {
+        } else if (event.keyCode == 13) { // Enter
             location.hash = '#' + $('#command').text();
             handle(location.hash);
             $('#command').text('');
+        } else if (event.keyCode == 9) { // Tab
+            var t = $('#command').text();
+            $('.cmddiv').each(function(i, el) {
+                if ($(el).attr('id').substring(0, t.length) == t) {
+                    $('#command').text($(el).attr('id'));
+                    return false;
+                }
+            });
+            return false;
         }
         if (event.charCode) {
             var c = String.fromCharCode(event.charCode);
